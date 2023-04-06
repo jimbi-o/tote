900faac2b840957c83c29c7e1ba11d881f073226
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2023-04-06 23:24:46 +0900

    HashMap moved to header file

---
+++ src/tote.cpp
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include <string.h>
+namespace tote {
+bool IsPrimeNumber(const uint32_t n) {
+  if (n <= 1) { return false; }
+  for (uint32_t i = 2; i * i <= n; i++) {
+    if (n % i == 0) { return false; }
+  }
+  return true;
+}
+uint32_t GetLargerOrEqualPrimeNumber(const uint32_t n) {
+  if (IsPrimeNumber(n)) { return n; }
+  if (n <= 2) { return 2; }
+  auto p = n + 1 + n % 2; // odd number larger than n.
+  while (!IsPrimeNumber(p)) {
+    p += 2;
+  }
+  return p;
+}
+bool IsCloseToFull(const uint32_t load, const uint32_t capacity) {
+  const float loadFactor = 0.65f;
+  return static_cast<float>(load) / static_cast<float>(capacity) >= loadFactor;
+}
+} // namespace tote
